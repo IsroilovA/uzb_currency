2572a442d8a722e4c70d702801fb58aa828f0c74
Alisher <aisroilov1029@gmail.com>
2024-05-28 19:46:12 +0500

    comments added

--- lib/service/api_helper.dart
+++ lib/service/api_helper.dart
@@ -4,11 +4,16 @@ import 'package:http/http.dart' as http;
 import 'package:uzb_currency/data/models/currency_rate.dart';
 import 'package:uzb_currency/service/helper_functions.dart';
 
+//class to fetch curencies from remote API
 class ApiHelper {
+  //fetch data fucntion
   static Future<List<CurrencyRate>?> fetchCurrencies(
       {required DateTime date, String? currency}) async {
+    //fromat date to insert into the link
     final insertedDate = dateFormatter.format(date);
+    //variable to hold the response
     final http.Response response;
+    //ckeck if currency was passed and insert if needed
     if (currency != null) {
       response = await http.get(Uri.parse(
           "https://cbu.uz/ru/arkhiv-kursov-valyut/json/$currency/$insertedDate/"));
@@ -16,9 +21,11 @@ class ApiHelper {
       response = await http.get(Uri.parse(
           "https://cbu.uz/ru/arkhiv-kursov-valyut/json/all/$insertedDate/"));
     }
-
+    //check for status code
     if (response.statusCode == 200) {
+      //decode the body received
       final List body = jsonDecode(response.body);
+      //convert to the list of CurrencyRate and return
       return body.map((e) => CurrencyRate.fromJson(e)).toList();
     } else {
       return null;
