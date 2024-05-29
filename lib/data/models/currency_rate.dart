f541fc7dd934f17ba31de778d4afabe876f80fc7
Alisher <aisroilov1029@gmail.com>
2024-05-29 15:07:07 +0500

    data model set up

--- lib/data/models/currency_rate.dart
+++ lib/data/models/currency_rate.dart
@@ -1,4 +1,6 @@
-import 'package:hive/hive.dart';
+import 'dart:convert';
+
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'currency_rate.g.dart';
@@ -21,11 +23,35 @@ class CurrencyRate {
   @HiveField(6)
   double difference;
 
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'code': code,
+      'curreny': currency,
+      'currencyName': currencyName,
+      'date': date,
+      'rate': rate,
+      'difference': difference,
+    };
+  }
+
+  factory CurrencyRate.fromMap(Map<String, dynamic> map) {
+    return CurrencyRate(
+      map['id'] as int,
+      map['code'] as int,
+      map['currency'] as String,
+      map['currencyName'] as String,
+      map['date'] as String,
+      map['rate'] as double,
+      map['difference'] as double,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
   CurrencyRate(this.id, this.code, this.currency, this.currencyName, this.date,
       this.rate, this.difference);
 
-  factory CurrencyRate.fromJson(Map<String, dynamic> json) =>
-      _$CurrencyRateFromJson(json);
-
-  Map<String, dynamic> toJson() => _$CurrencyRateToJson(this);
+  factory CurrencyRate.fromJson(String source) =>
+      CurrencyRate.fromMap(json.decode(source) as Map<String, dynamic>);
 }
--- lib/data/models/currency_rate.g.dart
+++ lib/data/models/currency_rate.g.dart
@@ -3,26 +3,57 @@
 part of 'currency_rate.dart';
 
 // **************************************************************************
-// JsonSerializableGenerator
+// TypeAdapterGenerator
 // **************************************************************************
 
-CurrencyRate _$CurrencyRateFromJson(Map<String, dynamic> json) => CurrencyRate(
-      (json['id'] as num).toInt(),
-      int.parse(json['Code'] as String),
-      json['Ccy'] as String,
-      json['CcyNm_EN'] as String,
-      json['Date'] as String,
-      double.parse(json['Rate'] as String),
-      double.parse(json['Diff'] as String),
-    );
+class CurrencyRateAdapter extends TypeAdapter<CurrencyRate> {
+  @override
+  final int typeId = 0;
 
-Map<String, dynamic> _$CurrencyRateToJson(CurrencyRate instance) =>
-    <String, dynamic>{
-      'id': instance.id,
-      'code': instance.code,
-      'currency': instance.currency,
-      'currencyName': instance.currencyName,
-      'date': instance.date,
-      'rate': instance.rate,
-      'difference': instance.difference,
+  @override
+  CurrencyRate read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
     };
+    return CurrencyRate(
+      fields[0] as int,
+      fields[1] as int,
+      fields[2] as String,
+      fields[3] as String,
+      fields[4] as String,
+      fields[5] as double,
+      fields[6] as double,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, CurrencyRate obj) {
+    writer
+      ..writeByte(7)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.code)
+      ..writeByte(2)
+      ..write(obj.currency)
+      ..writeByte(3)
+      ..write(obj.currencyName)
+      ..writeByte(4)
+      ..write(obj.date)
+      ..writeByte(5)
+      ..write(obj.rate)
+      ..writeByte(6)
+      ..write(obj.difference);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is CurrencyRateAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
